332b359cfd84fd9dac46225af374ad4bb6a460bd
Tanzim Ahmed <tanzimahmedsagar@gmail.com>
2026-05-16 20:21:56 -0600

    Create seadex.js

---
+++ tests/fixtures/seadex.js
@@ -0,0 +1,99 @@
+{
+  "page": 1,
+  "perPage": 1,
+  "totalItems": -1,
+  "totalPages": -1,
+  "items": [
+    {
+      "alID": 154587,
+      "collectionId": "3l2x9nxip35gqb5",
+      "collectionName": "entries",
+      "created": "2025-03-09 23:15:20.970Z",
+      "expand": {
+        "trs": [
+          {
+            "collectionId": "oiwizhmushn5qqh",
+            "collectionName": "torrents",
+            "created": "2025-04-19 21:49:29.688Z",
+            "dualAudio": true,
+            "files": [
+              { "length": 7500699108, "name": "Frieren Beyond Journey's End - S01E01 (BD Remux 1080p AVC FLAC AAC) [Dual Audio] [PMR].mkv" },
+              { "length": 7497267058, "name": "Frieren Beyond Journey's End - S01E02 (BD Remux 1080p AVC FLAC AAC) [Dual Audio] [PMR].mkv" }
+            ],
+            "groupedUrl": "",
+            "id": "yrq7x2vp6dlq1i0",
+            "infoHash": "143ed15e5e3df072ae91adaeb149973a887590dd",
+            "isBest": true,
+            "releaseGroup": "PMR",
+            "tags": [],
+            "tracker": "Nyaa",
+            "updated": "2025-07-26 15:05:59.431Z",
+            "url": "https://nyaa.si/view/1961373"
+          },
+          {
+            "collectionId": "oiwizhmushn5qqh",
+            "collectionName": "torrents",
+            "created": "2025-04-20 16:40:02.187Z",
+            "dualAudio": true,
+            "files": [
+              { "length": 7500699108, "name": "Frieren Beyond Journey's End - S01E01 (BD Remux 1080p AVC FLAC AAC) [Dual Audio] [PMR].mkv" },
+              { "length": 7497267058, "name": "Frieren Beyond Journey's End - S01E02 (BD Remux 1080p AVC FLAC AAC) [Dual Audio] [PMR].mkv" }
+            ],
+            "groupedUrl": "",
+            "id": "v0z6eytshc0l83e",
+            "infoHash": "<redacted>",
+            "isBest": true,
+            "releaseGroup": "PMR",
+            "tags": [],
+            "tracker": "AB",
+            "updated": "2025-07-26 15:05:59.506Z",
+            "url": "/torrents.php?id=86576&torrentid=1167293"
+          },
+          {
+            "collectionId": "oiwizhmushn5qqh",
+            "collectionName": "torrents",
+            "created": "2025-03-23 17:00:58.224Z",
+            "dualAudio": true,
+            "files": [
+              { "length": 3506804569, "name": "[LostYears] Frieren Beyond Journey's End - S01E01 (WEB 1080p x265 10-bit AAC Opus) [0F7F64F6].mkv" },
+              { "length": 3535154954, "name": "[LostYears] Frieren Beyond Journey's End - S01E02 (WEB 1080p x265 10-bit AAC Opus) [E5ECA664].mkv" }
+            ],
+            "groupedUrl": "",
+            "id": "ulmms43g0b5i44r",
+            "infoHash": "<redacted>",
+            "isBest": false,
+            "releaseGroup": "LostYears",
+            "tags": [],
+            "tracker": "AB",
+            "updated": "2025-07-26 15:05:59.585Z",
+            "url": "/torrents.php?id=86576&torrentid=1162986"
+          },
+          {
+            "collectionId": "oiwizhmushn5qqh",
+            "collectionName": "torrents",
+            "created": "2025-07-26 15:05:59.735Z",
+            "dualAudio": true,
+            "files": [
+              { "length": 3506804569, "name": "[LostYears] Frieren Beyond Journey's End - S01E01 (WEB 1080p x265 10-bit AAC Opus) [0F7F64F6].mkv" },
+              { "length": 3535154954, "name": "[LostYears] Frieren Beyond Journey's End - S01E02 (WEB 1080p x265 10-bit AAC Opus) [E5ECA664].mkv" }
+            ],
+            "groupedUrl": "",
+            "id": "hymgbjkx996v8x2",
+            "infoHash": "fb9ce1e001837de7662bd72b3fb79b3fea13d03f",
+            "isBest": false,
+            "releaseGroup": "LostYears",
+            "tags": [],
+            "tracker": "Nyaa",
+            "updated": "2025-07-26 15:05:59.735Z",
+            "url": "https://nyaa.si/view/1998171"
+          }
+        ]
+      },
+      "id": "aablda2m3i5pjxb",
+      "incomplete": false,
+      "notes": "Test fixture",
+      "trs": ["yrq7x2vp6dlq1i0", "v0z6eytshc0l83e", "ulmms43g0b5i44r", "hymgbjkx996v8x2"],
+      "updated": "2025-07-26 15:05:59.812Z"
+    }
+  ]
+}
